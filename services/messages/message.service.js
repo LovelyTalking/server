9b5d85453f9e6a03ab3ee9764f57c1d7f6e60b7a
BlockJJam <blockjjam99@gmail.com>
2020-10-02 13:31:44 +0900

    re-update

--- services/messages/message.service.js
+++ services/messages/message.service.js
@@ -36,16 +36,6 @@ const displayRoomList = async(req, res)=>{
 
     user_id = mongoose_type.ObjectId(search_option.user_id);
 
-    // const user_states = await UserStateInRoom.find({user_info: user_id, is_out:false},'room_info');
-    // if(!user_states)
-    //   return res.status(200).json({ display_room_list_success: true, room_list: []});
-    //
-    // const room_ids = user_states.map(state=>{ return state.room_info; });
-    //
-    // const room_list = await Room
-    //   .find({ _id: {$in:room_ids},users: user_id },'_id users user_state register_date update_date',{sort: '-update_date', skip: skip, limit: limit})
-    //   .populate({path: 'users', populate: {path: 'users' },select:'_id name email profile_image'})
-    //   .populate({path: 'user_state', populate:{path:'user_state'}});
 
     const {err, room_list} = await Room.findRoomListIncludedMyUserInfo(search_option, skip, limit);
     if(err) throw new CustomError(400, "룸 리스트 조회 모델함수에서 에러");
@@ -64,49 +54,7 @@ const createMessageRoom = async(req, res)=>{
     const other_user_id = mongoose_type.ObjectId(req.params.user_id);
 
     const {err,created_room} = await Room.createRoomAndUserState([my_user_id,other_user_id]);
-    // const found_room = await Room.findOne({users :[my_user_id, other_user_id]});
-    //
-    // if(found_room){
-    //   // find my_user_state, if(is_out === false) is_out = true;
-    //   let my_state = UserStateInRoom.findById(found_room.user_state[my_user_id]);
-    //   if(!my_state) throw new CustomError(400, "해당 룸에 유저의 상태를 찾을 수 없습니다.")
-    //
-    //   if(my_state.is_out){
-    //     my_state.is_out = false;
-    //     await my_state.save();
-    //   }
-    //
-    //   return res.status(200).json({create_room_success: true, created_room: found_room});
-    // }
-    // else{
-    //   // create room, create user_a state, user_b state
-    //
-    //   const users_in_room = { users: [my_user_id, other_user_id] };
-    //   const room_info = new IRoomDTO(users_in_room).getCreateRoomInfo();
-    //
-    //   let room = new Room(room_info);
-    //   let created_room = await room.save();
-    //
-    //   let state_info = {
-    //     room_info: created_room._id,
-    //     user_info: my_user_id
-    //   };
-    //   state_info = new IUserStateInRoomDTO(state_info).getCreateUserStateInfo();
-    //   let state = new UserStateInRoom(state_info);
-    //   const my_state = await state.save();
-    //
-    //   state_info.user_info = other_user_id;
-    //   state_info = new IUserStateInRoomDTO(state_info).getCreateUserStateInfo();
-    //   state = new UserStateInRoom(state_info);
-    //   const other_state = await state.save();
-    //
-    //   room.user_state.set(String(my_user_id), my_state._id);
-    //   room.user_state.set(String(other_user_id), other_state._id);
-    //
-    //   await room.save();
-    //
-    //   return res.status(200).json({create_room_success: true, created_room})
-    // }
+  
     if(err) throw new CustomError(400,"메신저 룸을 생성하는 모델함수에서 에러");
 
     return res.status(200).json({create_room_success:true, created_room});
